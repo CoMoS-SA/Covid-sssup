99b51c72747cd6d32d234c233955ac2dc24b7aab
Matteo Sostero <9999740+m-sostero@users.noreply.github.com>
2020-04-02 11:57:34 +0200

    Aggiunto ripartizioni regioni, aggiornato grafici
    Usa tabella esterna per ripartizione regioni (N-C-S, o NO, NE, C, S, I) come variabile aggiuntiva.
    
    Semplificato metodo selezione regioni più colpite per ripartizione, aggiustato grafico di conseguenza

--- data/Italy/descriptive-stats.R
+++ data/Italy/descriptive-stats.R
@@ -1,12 +1,24 @@
 library(tidyverse)
 library(lubridate)
+library(directlabels)
+library(scales)
 
+
+# Ripartizioni geografiche ----
+# Ripartizione a 5: Nord-ovest, Nord-est, Centro, Sud, Isole (definizione Istat)
+# Ripartizione a 3: Nord, Centro, Sud
+rip_regioni <- read_csv("ripartizione_regioni.csv", col_types = "ccc") %>% 
+  mutate(
+    ripartizione_3 = factor(ripartizione_3) %>% fct_inorder(),
+    ripartizione_5 = factor(ripartizione_5) %>% fct_inorder()
+  )
 
 # Scaricare tabella riepilogativa dati regionali ----
 dati_regionali <- read_csv(
   # Percorso tabella riepilogativa, aggiornata giornalmente
-  "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv",
-  # Schema tabella: specifica tipo di dato colonne
+  file = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-regioni/dpc-covid19-ita-regioni.csv",
+  # Schema tabella: specifica tipo di dato colonne;
+  # (La specifica non è strettamente necessaria, ma rende l'importazione della tabella più robusta a errori)
   col_types = cols(
     .default = col_double(),
     data = col_datetime(),
@@ -15,18 +27,26 @@ dati_regionali <- read_csv(
     denominazione_regione = col_character(),
     note_it = col_character(),
     note_en = col_character()
-  )) %>%
-  rename("regione" = "denominazione_regione")
+  )
+) %>%
+  rename("regione" = "denominazione_regione") %>%
+  # Aggungere ripartizioni delle regioni
+  left_join(rip_regioni, by = "regione") %>% 
+  # Data come giorno, rimuovendo ora del giorno
+  mutate(data = as.Date(data))
+
 
 
 # Calcolo incremento giornalieri
-dati_regionali <- dati_regionali %>% 
-  group_by(regione) %>% 
-  mutate(nuovi_deceduti      = deceduti - lag(deceduti,1),
-         nuovi_ospedalizzati = totale_ospedalizzati - lag(totale_ospedalizzati,1),
-         nuovi_terapia_int   = terapia_intensiva - lag(terapia_intensiva,1),
-         nuovi_tamponi       = tamponi - lag(tamponi,1),
-         nuovi_positivi      = totale_casi - lag(totale_casi,1)) %>% 
+dati_regionali <- dati_regionali %>%
+  group_by(regione) %>%
+  mutate(
+    nuovi_deceduti = deceduti - lag(deceduti, 1),
+    nuovi_ospedalizzati = totale_ospedalizzati - lag(totale_ospedalizzati, 1),
+    nuovi_terapia_int = terapia_intensiva - lag(terapia_intensiva, 1),
+    nuovi_tamponi = tamponi - lag(tamponi, 1),
+    nuovi_positivi = totale_casi - lag(totale_casi, 1)
+  ) %>%
   ungroup()
 
 
@@ -34,9 +54,10 @@ dati_regionali <- dati_regionali %>%
 
 theme_set(theme_bw())
 
-dati_regionali %>% 
-  ggplot(aes(x = data, y =  totale_casi)) +
-  geom_point() + geom_line() + 
+dati_regionali %>%
+  ggplot(aes(x = data, y = totale_casi)) +
+  geom_point() +
+  geom_line() +
   scale_y_log10() + annotation_logticks(sides = "l") +
   facet_wrap(~ regione, ncol = 5) +
   labs(
@@ -45,55 +66,61 @@ dati_regionali %>%
   )
 
 
-dati_regionali %>% 
-  group_by(regione) %>% 
-  arrange(regione, data) %>% 
-  mutate(diff_decessi = c(NA, diff(deceduti))) %>% 
-  ggplot(aes(x = data, y =  diff_decessi)) +
+dati_regionali %>%
+  group_by(regione) %>%
+  arrange(regione, data) %>%
+  mutate(diff_decessi = c(NA, diff(deceduti))) %>%
+  ggplot(aes(x = data, y = diff_decessi)) +
   geom_col() +
-  scale_y_log10() + annotation_logticks(sides = "l") +
-  facet_wrap(~ regione, ncol = 5 ) +
+  scale_y_log10() + 
+  annotation_logticks(sides = "l") +
+  facet_wrap(~regione, ncol = 5) +
   labs(
     title = "Differenza giornaliere decessi COVID-19", subtitle = "Per Regione o Provincia Autonoma",
     x = NULL, y = "Differenza decessi (scala logaritmica)"
   )
 
+
 ## 1a: Aree Geografiche ----
 
-nr_regioni_colpite <- 4 ## Seleziona numero di regioni colpite 
-df.cov %>% 
-  filter(data > "2020-03-03") %>% 
-  group_by(denominazione_regione) %>% 
-  summarise(tot_reg_lat = sum(totale_casi)) %>% 
-  ungroup() %>%
-  left_join(.,df.cov) %>% 
-  select(denominazione_regione,lat,long,tot_reg_lat) %>% distinct() %>% 
-  mutate(latitudine = if_else(lat > 44.44,"Nord",if_else(lat < 44.44 & lat > 41.2,"Centro","Sud"))) %>% 
-  group_by(latitudine) %>% 
-  top_n(n = nr_regioni_colpite,wt = tot_reg_lat) %>% 
-  ungroup() %>% 
-  left_join(.,df.cov) %>% 
-  select(data,lat,totale_casi,denominazione_regione,tot_reg_lat,latitudine) -> df.temp
+nr_regioni_colpite <- 4 ## Seleziona numero di regioni colpite
 
-## Cambia ordine fattori e seleziona colori per numero regioni considerate
-df.temp$latitudine  <- factor(df.temp$latitudine, levels = c("Nord","Centro","Sud"))
-colourCount         <- length(unique(df.temp$denominazione_regione))
-getPalette          <- colorRampPalette(brewer.pal(9, "Paired"))
 
-ggplot(df.temp,aes(x = data,y = totale_casi,group = denominazione_regione))+
-  geom_line(aes(color = denominazione_regione),size = 0.6)+
-  geom_point(aes(color = denominazione_regione, shape = denominazione_regione))+
-  facet_wrap(~latitudine)+
-  geom_dl(aes(label = denominazione_regione,color = denominazione_regione), method="smart.grid", cex = 0.8) +
-  theme_hc()+
-  scale_y_continuous(expand = c(0,0),trans = "log")+
-  scale_color_manual(values = getPalette(colourCount))+
-  theme(axis.ticks.y = element_blank(),
-        axis.text.y = element_blank(),
-        legend.position = "none")+
+# Seleziona regioni più colpite
+dati_regioni_colpite <- dati_regionali %>%
+  # nella data più recente
+  filter(data == max(data)) %>%
+  # raggruppa regioni per ogni ripartizione
+  group_by(ripartizione_3) %>%
+  # seleziona N regioni più colpite sulla base di totale_casi
+  top_n(n = nr_regioni_colpite, wt = totale_casi) %>% 
+  # assegna numeri progressivi alle regioni, 
+  # ripetuti tra ripartizioni diverse, per colori e forme
+  ungroup %>% arrange(ripartizione_3, desc(totale_casi)) %>% 
+  mutate(reg_colpita = 1:n() %>% factor()) %>% 
+  # Seleziona i dati per solo queste regioni
+  select(regione, reg_colpita) %>% 
+  left_join(dati_regionali, by = "regione") 
+
+
+ggplot(dati_regioni_colpite, aes(x = data, y = totale_casi, group = regione, label = regione, color = reg_colpita, shape = reg_colpita)) +
+  geom_line(size = 0.6) +
+  geom_point() +
+  # divide il grafico per ripartizione
+  facet_grid(~ ripartizione_3) +
+  # aggiunge nomi regioni accanto alle linee
+  geom_dl(method = "smart.grid", cex = 0.8) +
+  # scala verticale logaritmica, orizzontale date spaziate 1 settimanat
+  scale_y_log10() + annotation_logticks(sides = "l") +
+  scale_x_date(breaks = "1 week", date_labels = "%d %b") +
+  # scala colore linee e forme punti a N unità, ripetute tra ripartizioni 
+  scale_color_manual(values = brewer.pal(n = nr_regioni_colpite, 'Set2') %>% rep(3)) +
+  scale_shape_manual(values = c(0, 1, 2, 4) %>% rep(3)) +
+  theme(
+    legend.position = "none", 
+    axis.text.x = element_text(angle = 45, hjust = 1)
+    ) + 
   labs(
     title = "Curva Epidemica Per Regioni in Aree Geografiche", subtitle = "Scala logaritmica",
     x = NULL, y = "Totale Casi"
   )
-
-rm(df.temp)
